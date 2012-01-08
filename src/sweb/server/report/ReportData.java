0d4263f58e40a0d0a40b80d770d35c35b716c075
Gary Bak <garybak@gmail.com>
2012-01-07 20:52:16 -0500

    Added Report Data

---
+++ src/sweb/server/report/ReportData.java
@@ -0,0 +1,17 @@
+package sweb.server.report;
+
+import java.util.ArrayList;
+
+import sweb.server.controller.data.DataOrchestrator;
+import sweb.shared.model.SDataPoint;
+import sweb.shared.model.SDevice;
+import sweb.shared.model.logfile.LogNote;
+
+public class ReportData
+{
+    ArrayList<ArrayList<SDataPoint>> dataList = null;
+    ArrayList<SDevice> configList = null;
+    ArrayList<LogNote> noteList = null;
+
+    
+}
