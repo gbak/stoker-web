35b4593f5e725426d4b37f2200d3ba5251e6a5ce
Gary Bak <garybak@gmail.com>
2012-10-04 00:52:15 -0400

    Jackson type information

--- src/sweb/common/json/Device.java
+++ src/sweb/common/json/Device.java
@@ -1,7 +1,15 @@
 package sweb.common.json;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value=Probe.class, name="probe"),
+    @JsonSubTypes.Type(value=PitProbe.class, name="pit"),
+    @JsonSubTypes.Type(value=Blower.class, name="fan")
+})
 public class Device
 {
     @JsonProperty(value="id")
