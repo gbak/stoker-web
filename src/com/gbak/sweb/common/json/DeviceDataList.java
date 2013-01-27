0ccedede9065134a553396afb5126192addf7b18
Gary Bak <garybak@gmail.com>
2013-01-27 01:13:41 -0500

    Added blower list

--- src/com/gbak/sweb/common/json/DeviceDataList.java
+++ src/com/gbak/sweb/common/json/DeviceDataList.java
@@ -7,12 +7,14 @@ import java.util.Date;
 public class DeviceDataList //extends ArrayList<Device>
 {   
     public ArrayList<Device> devices;
+    public ArrayList<Device> availableBlowers;
     public Date receivedDate;
     public LogItemCount logCount;
     
     public DeviceDataList()
     {
         devices = new ArrayList<Device>();
+        availableBlowers = new ArrayList<Device>();
     }
     
 }
